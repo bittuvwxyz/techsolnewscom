b7f10958293faf668ccc0aeff06be511cddf372d
bittuvwxyz <178542980+bittuvwxyz@users.noreply.github.com>
2026-07-26 13:42:37 +0530

    404 implemented

--- 404.php
+++ 404.php
@@ -1,3 +1,14 @@
 <?php
+http_response_code(404);
+require_once __DIR__ . '../config/db.php';
+require_once __DIR__ . '../config/config.php';
+require_once __DIR__ . '../includes/functions.php';
+require_once __DIR__ . '../includes/header.php';
+?>
 
+<h1>404 - Page Not Found</h1>
+<p>Sorry, the page you are looking for does not exist. Please check the URL or return to the <a href="/">homepage</a>.</p>  
+
+<?php
+require_once __DIR__ . '../includes/footer.php';
 ?>
